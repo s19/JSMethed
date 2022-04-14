a3a5773c97a142c878a188524b8d716b56e7f797
s19 <korsar_91sh@mail.ru>
2022-04-15 00:54:39 +0600

    Fix total price

--- Lesson10/cart.js
+++ Lesson10/cart.js
@@ -12,12 +12,6 @@
             });
             return totalPrice;
         },
-        // Принимает три параметра:
-        // название товара
-        // цену товара
-        // количество товара (опциональный параметр, по умолчанию 1 товар)
-        // этот метод формирует объект из полученных параметров и добавляет его в свойство items
-        // так же вызывает все необходимые методы чтобы свойства count и totalPrice были актуальные
         add(name, price, count = 1){
             let item = {
                 name: name,
@@ -25,14 +19,14 @@
                 count: count,
             }
             this.items.push(item);
-            this.increaseCount(count); 
-            //this.calculateItemPrice();          
+            this.increaseCount(count);         
         },
-        // Увеличивает свойство count на это число
         increaseCount(count){
             this.count += count;
         },
-        // Очищает полностью нашу корзину, возвращает все значения в изначальные
+        calculateItemPrice(){
+            return `Total price ${this.TotalPrice}`;
+        },
         clear(){
             this.items = [], this.totalPrice = 0, this.count = 0;
         },
@@ -46,8 +40,9 @@
     cart.add("Maseratti S7", 150000000, 2);
     cart.add("BMW X6", 587000000, 3);
     cart.add("Daewoo Matiz", 107000, 7);
-    cart.print();
+    // cart.print();
 
-    cart.clear();
-    cart.print();
+    // cart.clear();
+    // cart.print();
+    console.log(cart.calculateItemPrice());
 }
---
+++ Lesson11/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+{
+    const typesConsole = "Console Types";
+
+    /// Timer
+    console.timer('StartTimer');
+
+    console.error(typesConsole);
+    console.debug(typesConsole);
+    console.warn(typesConsole);
+    console.info(typesConsole);
+    console.trace(typesConsole);
+
+    /// Timer
+    console.timerEnd('EndTimer');
+}
